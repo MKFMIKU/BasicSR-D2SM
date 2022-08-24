d17750e78ab25e56efd07ab44b7251209825c129
Kangfu Mei <mikumkf@gmail.com>
2022-08-24 21:46:43 +0000

    update d2sm implementation

--- basicsr/losses/basic_loss.py
+++ basicsr/losses/basic_loss.py
@@ -251,3 +251,140 @@ class PerceptualLoss(nn.Module):
         features_t = features.transpose(1, 2)
         gram = features.bmm(features_t) / (c * h * w)
         return gram
+
+@LOSS_REGISTRY.register()
+class D2SMLoss(nn.Module):
+    """Deep Semantic Statistics Matching (D2SM) Denoising Network
+
+    Args:
+        layer_weights (dict): The weight for each layer of vgg feature.
+            Here is an example: {'conv5_4': 1.}, which means the conv5_4
+            feature layer (before relu5_4) will be extracted with weight
+            1.0 in calculating losses.
+        vgg_type (str): The type of vgg network used as feature extractor.
+            Default: 'vgg19'.
+        use_input_norm (bool):  If True, normalize the input image in vgg.
+            Default: True.
+        range_norm (bool): If True, norm images with range [-1, 1] to [0, 1].
+            Default: False.
+    """
+
+    def __init__(self,
+                 layer_weights,
+                 K=224,
+                 Q=256,
+                 vgg_type='vgg19',
+                 use_input_norm=True,
+                 range_norm=False):
+        super(D2SMLoss, self).__init__()
+        self.layer_weights = layer_weights
+        self.vgg = VGGFeatureExtractor(
+            layer_name_list=list(layer_weights.keys()),
+            vgg_type=vgg_type,
+            use_input_norm=use_input_norm,
+            range_norm=range_norm)
+        self.K = K
+        self.Q = Q
+
+    def init_queue_buffer(self, feature_dim):
+        # create the queue
+        self.register_buffer("data_queue", torch.randn(self.K, feature_dim))
+        self.register_buffer("label_queue", torch.randn(self.K, feature_dim))
+        self.data_queue = nn.functional.normalize(self.data_queue, dim=1)
+        self.label_queue = nn.functional.normalize(self.label_queue, dim=1)
+
+        self.register_buffer("data_queue_ptr", torch.zeros(1, dtype=torch.long))
+        self.register_buffer("label_queue_ptr", torch.zeros(1, dtype=torch.long))
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue_data(self, keys):
+        if 'data_queue' not in self:
+            self.init_queue_buffer(sum(keys.shape))
+        # gather keys before updating queue
+        batch_size = keys.shape[0]
+
+        ptr = int(self.data_queue_ptr)
+        assert self.K % batch_size == 0  # for simplicity
+
+        # replace the keys at ptr (dequeue and enqueue)
+        self.data_queue[ptr : ptr + batch_size, :] = keys
+        ptr = (ptr + batch_size) % self.K  # move pointer
+
+        self.data_queue_ptr[0] = ptr
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue_label(self, keys):
+        # gather keys before updating queue
+        batch_size = keys.shape[0]
+
+        ptr = int(self.label_queue_ptr)
+        assert self.K % batch_size == 0  # for simplicity
+
+        # replace the keys at ptr (dequeue and enqueue)
+        self.label_queue[ptr : ptr + batch_size, :] = keys
+        ptr = (ptr + batch_size) % self.K  # move pointer
+
+        self.label_queue_ptr[0] = ptr
+
+        def _encode(self, x):
+            x = self.vgg(x)
+            x = x.view(x.size(0), -1)
+            return F.normalize(x, dim=1)
+
+    def _divergence(self, feat_s, feat_t, eps=1e-6):
+        feat_s_norm = torch.sqrt(torch.sum(feat_s ** 2, dim=1, keepdim=True))
+        feat_s = feat_s / (feat_s_norm + eps)
+        feat_s[feat_s != feat_s] = 0
+
+        feat_t_norm = torch.sqrt(torch.sum(feat_t ** 2, dim=1, keepdim=True))
+        feat_t = feat_t / (feat_t_norm + eps)
+        feat_t[feat_t != feat_t] = 0
+
+        # Calculate the cosine similarity
+        feat_s_cos_sim = torch.mm(feat_s, feat_s.transpose(0, 1))
+        feat_t_cos_sim = torch.mm(feat_t, feat_t.transpose(0, 1))
+
+        # Scale cosine similarity to [0,1]
+        feat_s_cos_sim = (feat_s_cos_sim + 1.0) / 2.0
+        feat_t_cos_sim = (feat_t_cos_sim + 1.0) / 2.0
+
+        # Transform them into probabilities
+        feat_s_cond_prob = feat_s_cos_sim / torch.sum(
+            feat_s_cos_sim, dim=1, keepdim=True
+        )
+        feat_t_cond_prob = feat_t_cos_sim / torch.sum(
+            feat_t_cos_sim, dim=1, keepdim=True
+        )
+
+        # Calculate the KL-divergence
+        divergence = torch.mean(
+            feat_t_cond_prob
+            * torch.log((feat_t_cond_prob + eps) / (feat_s_cond_prob + eps))
+        )
+        return divergence
+
+    def forward(self, x, gt):
+        """Forward function.
+
+        Args:
+            x (Tensor): Input tensor with shape (n, c, h, w).
+            gt (Tensor): Ground-truth tensor with shape (n, c, h, w).
+
+        Returns:
+            Tensor: Forward results.
+        """
+        # extract vgg features
+        x_features = self.vgg(x)
+        gt_features = self.vgg(gt.detach())
+
+        # calculate perceptual loss
+        if self.perceptual_weight > 0:
+            for k in x_features.keys():
+                self._dequeue_and_enqueue_data(x_features[k])
+                with torch.no_grad():
+                    self._dequeue_and_enqueue_label(gt_features[k])
+                divergence = self._divergence(
+                    self.data_queue, self.label_queue.clone().detach()
+                )
+
+        return divergence
